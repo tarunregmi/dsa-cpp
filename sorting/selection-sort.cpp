4d11a30a6feca420c909bb145dc9ed86856b0e61
tarunregmi <tarunregmi@gmail.com>
2023-11-26 17:01:59 +0545

    selection sort

---
+++ sorting/selection-sort.cpp
@@ -0,0 +1,26 @@
+#include <type_traits>
+
+/**
+ * @param array array (int || char || float || double) to be sort using Bubble Sort algorithm
+ * @param n lenght of array
+ */
+template <typename T, typename = std::enable_if_t<
+	std::is_same<T, int>::value || 
+	std::is_same<T, char>::value ||
+	std::is_same<T, float>::value ||
+	std::is_same<T, double>::value
+>>
+void SelectionSort(T array[], int n) {
+	for (int i = 0; i < n; i++) {
+        int smallestItemIndex = i;
+        for (int j = i+1; j < n; j++) {
+            if (array[j] < array[smallestItemIndex]) smallestItemIndex = j;
+        }
+
+        if (smallestItemIndex != i) {
+            int tmp = array[i];
+            array[i] = array[smallestItemIndex];
+            array[smallestItemIndex] = tmp;
+        }
+    }
+}
