d5ea10a01d26a21b90ca4636b3a3df5a3a925ee7
tarunregmi <tarunregmi@gmail.com>
2023-11-27 13:59:25 +0545

    searching

---
+++ searching/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// import your sorting function here
+
+int main() {
+	int unsortedArray[] = {0};
+	int sortedArray[] = {-1, 5, 6, 18, 19, 25, 46, 78, 102, 114};
+
+	int length = sizeof(sortedArray) / sizeof(sortedArray[0]);
+	
+	int target = 18;
+	int result = -1; // call sorting function here
+
+	if (result == -1) std::cout << "not found" << std::endl;
+	else std::cout << target << " is at index: " << result << std::endl;
+	
+	return 0;
+}
