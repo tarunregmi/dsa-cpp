06605793ac2ef1464032b14f5c754b62cd6516bf
tarunregmi <tarunregmi@gmail.com>
2023-11-29 12:55:30 +0545

    Tower if Hanoi problem

---
+++ recursion/toh.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void toh(int n, char src, char help, char dest) {
+	if (n == 1) {
+		std::cout << "transfer disk " << n << " from " << src << " to " << dest << std::endl;
+		return;
+	}
+
+	toh(n-1, src, dest, help);
+	std::cout << "transfer disk " << n << " from " << src << " to " << dest << std::endl;
+	toh(n-1, help, src, dest);
+}
+
+int main() {
+	int n = 3;
+
+	toh(n, 'S', 'H', 'D');
+
+	return 0;
+}
