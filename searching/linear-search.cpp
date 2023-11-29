acf0009f1ad031d39814923970b33ee59371f8f4
tarunregmi <tarunregmi@gmail.com>
2023-11-29 12:41:49 +0545

    m

--- searching/linear-search.cpp
+++ searching/linear-search.cpp
@@ -13,6 +13,6 @@ template <typename T, typename = std::enable_if_t<
 	std::is_same<T, double>::value
 >>
 int LinearSearch(T array[], int n, T target) {
-    for (int i=0; i < n; i++) if (target == array[i]) return i;
-    return -1; // if target not found
+	for (int i=0; i < n; i++) if (target == array[i]) return i;
+	return -1; // if target not found
 }
